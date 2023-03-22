fc2f5dba3f83106587eaa8d40be0a9821ec4b0f0
omar546 <omer.mkawy.om@gmail.com>
2023-03-22 02:59:05 +0200

    catch lower's 🧤

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * islower - check for lower case.
+ * @c: input to be checked.
+ * Return: 1 if c is lowercase, Returns 0 otherwise.
+*/
+int islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+else
+return (0);
+}
