ec3ea0907ecb37801f0dd6b7f1c1503464c62f3d
omar546 <omer.mkawy.om@gmail.com>
2023-03-22 02:45:35 +0200

    fix for loop 🔧

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,7 +6,8 @@
 void print_alphabet_x10(void)
 {
 char letter;
-for (int i = 0 ; i < 11 ; i++)
+int i;
+for (i = 0 ; i < 11 ; i++)
 {
 for (letter = 'a'; letter <= 'z'; letter++)
 _putchar((char)letter);
