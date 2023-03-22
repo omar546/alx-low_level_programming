34a4eb18d79a6ce26cf9662d8b06263345fb6704
omar546 <omer.mkawy.om@gmail.com>
2023-03-22 02:37:45 +0200

    Alx Task 0-1-2 🚀

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ */
+void print_alphabet(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar((char)letter);
+_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_alphabet_x10- prints the alphabet in lowercase X10 times,
+*followed by a new line..
+*/
+void print_alphabet_x10(void)
+{
+char letter;
+for (int i = 0 ; i < 11 ; i++)
+{
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar((char)letter);
+_putchar('\n');
+}
+}
