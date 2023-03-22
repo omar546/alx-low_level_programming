e88ed6df88aae471aa103edf7c671c2f9d9b1d0d
omar546 <omer.mkawy.om@gmail.com>
2023-03-22 03:10:57 +0200

    alpha-checker 🔤

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
  * @c: the input to checked
  * Return: 1 if the input c is alphabetic 0 otherwise
  */
-int _isalpha(int c);
+int _isalpha(int c)
 {
 if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 return (1);
