4991726bd76e6b6f7aa8e96482bee770a9bff60c
omar546 <omer.mkawy.om@gmail.com>
2023-03-22 04:11:12 +0200

    last digit is : 🔢

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,11 +2,11 @@
 /**
  * print_last_digit - print last digit of n.
  * @n: the input number.
- * Return: always 0.
+ * Return: last digit of n.
  */
 int print_last_digit(int n)
 {
-int last = _abs(n) % 10;
+int last = n % 10;
 _putchar(last);
 return (last);
 }
