8cfb0e245de3ed4a4b120341fc2bb66378ee528e
omar546 <omer.mkawy.om@gmail.com>
2023-07-07 23:48:22 +0200

    static

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # a script that creates a static library
-# called liball.a from all the .c files
+# called libmy.a from all the .c files
 # that are in the current directory
 
 gcc -c *.c
-ar rc liball.a *.o
+ar rc libmy.a *.o
 echo y | rm -I *.o
