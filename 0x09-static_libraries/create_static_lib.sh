f36e29ad1e8686da94d122350560e70dbec0153d
omar546 <omer.mkawy.om@gmail.com>
2023-07-07 23:58:12 +0200

    static

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -4,6 +4,14 @@
 # called liball.a from all the .c files
 # that are in the current directory
 
+# Compile each .c file into .o object files
 gcc -c *.c
-ar rc liball.a *.o
-echo y | rm -I *.o
+
+# Create the static library liball.a from the .o object files
+ar rcs liball.a *.o
+
+# List the contents of the created library
+ar -t liball.a
+
+# Remove the temporary .o object files
+rm *.o
