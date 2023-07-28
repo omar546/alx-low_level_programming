f6eac7c68fd813cc59f54adab748b8aeafabd8d5
omar546 <omer.mkawy.om@gmail.com>
2023-07-28 08:03:19 +0200

    rem f

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,18 @@
 section .data
-    hello_string db 'Hello, Holberton', 0   ; Null-terminated string for printf
+    hello_string db 'Hello, Holberton', 10   ; Null-terminated string for printing with new line (10)
 
 section .text
-    global main
+    global _start
 
-extern printf
-
-main:
-    ; Call printf to print the hello_string
-    mov rdi, hello_string    ; Load the address of the string into rdi
-    call printf             ; Call the printf function
+_start:
+    ; Write the hello_string to stdout (file descriptor 1)
+    mov rax, 1              ; Syscall number for sys_write (1 in 64-bit)
+    mov rdi, 1              ; File descriptor for stdout (1)
+    mov rsi, hello_string   ; Address of the string to print
+    mov rdx, 16             ; Length of the string (including null terminator and new line)
+    syscall                 ; Invoke the syscall to write the string to stdout
 
     ; Exit the program
-    mov rax, 60             ; Syscall number for exit (60 in 64-bit)
+    mov rax, 60             ; Syscall number for sys_exit (60 in 64-bit)
     xor rdi, rdi            ; Exit status 0
     syscall                 ; Invoke the syscall to exit the program
-
