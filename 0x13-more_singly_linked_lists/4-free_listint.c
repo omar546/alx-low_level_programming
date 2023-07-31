79bc448d327e0bba465b4b43f5727b9c5dfcc1b8
omar546 <omer.mkawy.om@gmail.com>
2023-07-31 19:15:30 +0200

    check

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -9,12 +9,11 @@
 
 void free_listint(listint_t *head)
 {
-	listint_t *nav = head, *nav2;
+    listint_t *nav = head, *nav2;
 
-	while (nav)
-	{
-		nav2 = nav->next;
-		free(nav);
-		nav = nav2;
-	}
+    while (nav) {
+        nav2 = nav->next;
+        free(nav);
+        nav = nav2;
+    }
 }
