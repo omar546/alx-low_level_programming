985f99d65cb29d310e3eddca7d11f9959b249447
omar546 <omer.mkawy.om@gmail.com>
2023-08-08 00:20:39 +0200

    c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,10 +14,10 @@
 int create_file(const char *filename, char *text_content)
 {
     int fd, bytes_written;
-    
+
     if (filename == NULL)
         return (-1);
-    
+
     fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
     if (fd == -1)
         return (-1);
