656f6ee5e914f5d17e1800ec7573940e21c7fd7c
omar546 <omer.mkawy.om@gmail.com>
2023-11-04 01:46:42 +0200

    ho

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,7 +11,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
-		return (NULL);
+    {
+        return (NULL);
+    }
 	hash_table->size = size;
 	hash_table->array = calloc(size, sizeof(hash_node_t**));
 	return (hash_table);
