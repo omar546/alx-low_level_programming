70d5a82bc9abf4823222d45cc4eba48d84d4e2f6
omar546 <omer.mkawy.om@gmail.com>
2023-11-03 19:48:29 +0200

    e

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,7 +6,7 @@
  * Return: pointer to the new hash table
  */
 
-hash_table_t *hash_table_create(unsigned long int sizee)
+hash_table_t *hash_table_create(unsigned long int size)
 {
 hash_table_t *hash_table;
 unsigned long int j;
@@ -14,14 +14,14 @@ unsigned long int j;
 hash_table = malloc(sizeof(hash_table_t));
 if (hash_table == NULL)
 return (NULL);
-hash_table->size = sizee;
-hash_table->array = malloc(sizee * sizeof(hash_node_t *));
+hash_table->size = size;
+hash_table->array = malloc(size * sizeof(hash_node_t *));
 if (hash_table->array == NULL)
 {
 free(hash_table);
 return (NULL);
 }
-for (j = 0; j < sizee; j++)
+for (j = 0; j < size; j++)
 hash_table->array[j] = NULL;
 return (hash_table);
 }
