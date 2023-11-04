17c1ec71d5dcb9db86665f6bbf99c66bc532a097
omar546 <omer.mkawy.om@gmail.com>
2023-11-04 02:22:00 +0200

    5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,30 +1,32 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_print - prints the entire hash table
- * @ht: a pointer to the hash table
+ * hash_table_print - prints the keys and values of the hash table
+ *
+ * @ht: pointer to the hash table
+ * Return: no return
  */
 void hash_table_print(const hash_table_t *ht)
 {
-    unsigned long int i;
-    hash_node_t *node;
-    int first = 1;
+	unsigned long int i;
+	hash_node_t *tmp;
+	char *sep;
 
-    if (ht == NULL)
-        return;
+	if (ht == NULL)
+		return;
 
-    printf("{");
-    for (i = 0; i < ht->size; i++)
-    {
-        node = ht->array[i];
-        while (node != NULL)
-        {
-            if (!first)
-                printf(", ");
-            printf("'%s': '%s'", node->key, node->value);
-            first = 0;
-            node = node->next;
-        }
-    }
-    printf("}\n");
+	printf("{");
+	sep = "";
+
+	for (i = 0; i < ht->size; i++)
+	{
+		tmp = ht->array[i];
+		while (tmp != NULL)
+		{
+			printf("%s'%s': '%s'", sep, tmp->key, tmp->value);
+			sep = ", ";
+			tmp = tmp->next;
+		}
+	}
+	printf("}\n");
 }
