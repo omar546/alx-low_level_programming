98a206fd65e3d2354e01249b52feb43a562c307d
omar546 <omer.mkawy.om@gmail.com>
2023-11-04 02:12:28 +0200

    try

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -8,24 +8,26 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-	unsigned long int i;
-	hash_node_t *temp;
-	hash_node_t *temp2;
+    unsigned long int i;
+    hash_node_t *temp;
+    hash_node_t *temp2;
 
-	if (ht == NULL)
-		return;
+    if (ht == NULL)
+        return;
 
-	for (i = 0; i < ht->size; i++)
-	{
-		temp = ht->array[i];
-		while ((temp2 = temp) != NULL)
-		{
-			temp = temp->next;
-			free(temp2->key);
-			free(temp2->value);
-			free(temp2);
-		}
-	}
-	free(ht->array);
-	free(ht);
+    for (i = 0; i < ht->size; i++)
+    {
+        temp = ht->array[i];
+        while (temp != NULL)
+        {
+            temp2 = temp;
+            temp = temp->next;
+            free(temp2->key);
+            free(temp2->value);
+            free(temp2);
+        }
+    }
+
+    free(ht->array);
+    free(ht);
 }
