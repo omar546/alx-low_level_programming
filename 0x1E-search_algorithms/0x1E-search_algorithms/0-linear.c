0d40ff9ff0535b77094a4fe009b450d7d2373079
omar546 <omer.mkawy.om@gmail.com>
2024-02-05 23:30:51 +0200

    search with me

---
+++ 0x1E-search_algorithms/0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,25 @@
+#include "search.h"
+#include <unistd.h>
+
+/**
+ * linear_search
+ * @array: array to search the value in
+ * @size: size of the array
+ * @value: value to look up
+ * Return: index or -1
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+
+	if (!array || size == 0)
+		return (-1);
+	while (i < size)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,43 @@
+#include "search.h"
+
+/**
+ * binary_search
+ * @array: array to search the value in
+ * @size: size of the array
+ * @value: value to look up
+ * Return: index or -1
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	int i, left, right;
+
+	if (array == NULL)
+		return (-1);
+
+	left = 0;
+	right = size - 1;
+
+	while (left <= right)
+	{
+		int mid = left + (right - left) / 2;
+
+		printf("Searching in array: ");
+		for (i = left; i <= right; i++)
+		{
+			if (i == left)
+				printf("%d", array[i]);
+			else
+				printf(", %d", array[i]);
+		}
+		printf("\n");
+
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] < value)
+			left = mid + 1;
+		else
+			right = mid - 1;
+	}
+
+	return (-1);
+}
